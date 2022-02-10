bf4564ad3437db074eb891497fdf7698f58b7d87
NikolaevAnton <mjop41@gmail.com>
2022-02-10 23:22:00 +0300

    Initial Commit

---
+++ NikolaevAA_3.7/NikolaevAA_3_7App.swift
@@ -0,0 +1,17 @@
+//
+//  NikolaevAA_3_7App.swift
+//  NikolaevAA_3.7
+//
+//  Created by Anton Nikolaev on 10.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct NikolaevAA_3_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
